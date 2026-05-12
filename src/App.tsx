e69d32e031ba4e834e69a66ac4f4c5b6b5dbd2ee
[dyad] <pedrobarroscastro96@gmail.com>
2026-05-12 20:03:11 -0300

    Reverted all changes back to version d231d0bbe9143eac53c24bd37123f3098164f181

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Tasks from "./pages/Tasks";
-import Login from "./pages/Login";
-import Index from "./pages/Index";
-import HelloIA from "./pages/HelloIA";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public pages */}
-        <Route path="/" element={<Index />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/hello-ia" element={<HelloIA />} />
-
-        {/* Protected page */}
-        <Route path="/tasks" element={<Tasks />} />
-
+        {/* Root path now shows the Tasks page directly */}
+        <Route path="/" element={<Tasks />} />
         {/* Fallback for any unknown routes */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
